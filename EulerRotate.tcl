f9e88345d564758940de7d32d3be4eba21235d99
criosx <mail@frank-heinrich.net>
2017-01-03 15:40:24 -0500

    correct EulerRotate.tcl for 4PI rotations

--- EulerRotate.tcl
+++ EulerRotate.tcl
@@ -1,6 +1,6 @@
-set filename "1OGS"
-set betastart -90
-set betaend    90
+set filename "protein"
+set betastart  0
+set betaend    180
 set betainc    5
 set gammastart 0
 set gammaend   360
@@ -10,15 +10,18 @@ for {set beta $betastart} {$beta <= $betaend} {set beta [expr {$beta + $betainc}
     for {set gamma $gammastart} {$gamma < $gammaend} {set gamma [expr {$gamma + $gammainc}]} {
         set fname $filename
         append fname ".pdb"
-        mol new $fname
-        set repres [atomselect top all]
-        $repres move [transaxis z $gamma]
-        $repres move [transaxis x $beta]
-        #rotate z by $gamma
-        #rotate x by $beta
+        animate read pdb $fname waitfor all
+        set nf [molinfo top get numframes]
+        for {set fr 0} {$fr < $nf} {incr fr} {
+            set repres [atomselect top all frame $fr]
+            $repres move [transaxis z $gamma]
+            $repres move [transaxis x $beta]
+            #rotate z by $gamma
+            #rotate x by $beta
+        }
         set fname $filename
         append fname "_tilt" $beta "_orie" $gamma ".pdb"
-        $repres writepdb $fname
-        mol delete all
+        animate write pdb $fname waitfor all
+        animate delete all
     }
 }
